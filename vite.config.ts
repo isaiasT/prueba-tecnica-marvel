d21f7f2c513493888539d657ce3eb12f705465ef
Isaias Tortosa <isaiasvillena92@gmail.com>
2024-07-25 18:20:22 +0200

    feat: Configure dev and prod modes to minify assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import terser from '@rollup/plugin-terser';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const isDevelopment = mode === 'development';
+
+    return {
+        plugins: [react()],
+        build: {
+            minify: isDevelopment ? false : 'terser',
+            rollupOptions: {
+                output: {
+                    sourcemap: isDevelopment,
+                },
+                plugins: isDevelopment
+                    ? []
+                    : [
+                          terser({
+                              compress: {
+                                  drop_console: true,
+                              },
+                          }),
+                      ],
+            },
+        },
+    };
 });
