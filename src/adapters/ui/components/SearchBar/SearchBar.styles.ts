c11d58a7d4f4cffd232756245103355f0c1dc2b9
Isaias Tortosa <isaiasvillena92@gmail.com>
2024-07-27 17:02:17 +0200

    feat: Implement CharacterCard and CharacterCardGroup

--- src/adapters/ui/components/SearchBar/SearchBar.styles.ts
+++ src/adapters/ui/components/SearchBar/SearchBar.styles.ts
@@ -1,10 +1,9 @@
 import { styled } from 'styled-components';
 
 export const SearchBarContainer = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 77px;
     padding: 12px 48px 12px 48px;
-    box-sizing: border-box;
     display: flex;
     flex-direction: column;
     gap: 12px;
--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,9 +1,31 @@
+import CharacterCard from '../../adapters/ui/components/CharacterCard/CharacterCard';
+import CharacterCardGroup from '../../adapters/ui/components/CharacterCardGroup/CharacterCardGroup';
 import SearchBar from '../../adapters/ui/components/SearchBar/SearchBar';
 
 function Page() {
     return (
         <>
             <SearchBar />
+            <CharacterCardGroup>
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={false} imageSrc="/magneto.png" />
+                <CharacterCard name="Magneto" isFavorite={true} imageSrc="/magneto.png" />
+            </CharacterCardGroup>
         </>
     );
 }
