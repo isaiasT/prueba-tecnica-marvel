a053b857a27c2ef4273e770e9f02a99af7575eeb
Isaias Tortosa <isaiasvillena92@gmail.com>
2024-07-29 03:09:37 +0200

    chore: change all calls from http to https

--- src/adapters/ui/components/CharacterCardGroup/CharacterCardGroup.styles.ts
+++ src/adapters/ui/components/CharacterCardGroup/CharacterCardGroup.styles.ts
@@ -4,7 +4,6 @@ import { device } from '../../global-styles/breakpoints';
 
 export const CharacterCardGroupContainer = styled.div`
     width: 100%;
-    height: 640px;
     padding: 0px 48px 0px 48px;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
